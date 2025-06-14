8f4a789fa1990e6dee92c4f370f4c207026c8941
omarmouslam42 <abomoslamomar@gmail.com>
2025-06-14 21:36:26 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -5,26 +5,24 @@ import { connectDB } from "./lib/connectDB.js";
 import authRoutes from "./modules/auth/auth.router.js";
 import transcriptionRoutes from "./modules/Transcription/Transcription.routes.js";
 import serverlessHttp from "serverless-http";
-const app = express();
-connectDB();
 
-const Port = process.env.PORT || 4000;
+const app = express();
+connectDB();                               // اتصال وحيد
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/test", (req, res) => {
-  res.send("✅ Route works");
-});
 app.get("/", (req, res) => {
   res.send("🚀 Spokify backend is running.");
 });
-app.get("/favicon.ico", (req, res) => res.status(204).end());
+app.get("/test", (_, res) => res.send("✅ Route works"));
+app.get("/favicon.ico", (_, res) => res.status(204).end());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/", transcriptionRoutes);
+app.use("/api", transcriptionRoutes);       // لاحظ بدون "/" زائدة
 
-export default serverlessHttp(app);
+export default serverlessHttp(app);         // default export مطلوب فى Vercel
 
 
 // app.listen(Port, () => { 
