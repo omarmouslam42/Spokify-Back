9ebd86a13c7e34f82754e31b0f772c8db1e352cd
omarmouslam42 <abomoslamomar@gmail.com>
2025-06-14 20:46:02 +0300

    test

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+app.get("/test", (req, res) => {
+  res.send("✅ Route works");
+});
 app.get("/", (req, res) => {
   res.send("🚀 Spokify backend is running.");
 });
