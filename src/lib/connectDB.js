16731076c9de133059e406e39e86b256d14b4979
omarmouslam42 <abomoslamomar@gmail.com>
2025-06-14 20:10:20 +0300

    add isConnected

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-
+ let isConnected = false;
 export const connectDB = async () => {
+  if (isConnected) return;
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected successfully ${conn.connection.host} `);
+     isConnected = true;
   } catch (error) {
     console.log("MongoDB connection failed", error);
     process.exit(1);
